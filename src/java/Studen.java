df9e6c2884c53d1f8812968b4bb3fd6e601a1c29
DELL <DELL@DESKTOP-DOAN-HIEU-DTU>
2017-11-28 08:30:48 +0700

    First viesion

---
+++ src/java/Studen.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author DELL
+ */
+public class Studen {
+    private String studenId;
+    private String studenName;
+//
+    public String getStudenId() {
+        return studenId;
+    }
+
+    public void setStudenId(String studenId) {
+        this.studenId = studenId;
+    }
+
+    public String getStudenName() {
+        return studenName;
+    }
+
+    public void setStudenName(String studenName) {
+        this.studenName = studenName;
+    }
+    
+}
